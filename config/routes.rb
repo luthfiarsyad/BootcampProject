fbc189e32c797192d3601e948f4fc79bdb14bac7
luthfiarsyad <62200039+luthfiarsyad@users.noreply.github.com>
2022-04-15 12:58:03 +0700

    Adding user, category, product and order detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :order_details
+  resources :users
+  resources :products
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root ""
   # root "articles#index"
 end
